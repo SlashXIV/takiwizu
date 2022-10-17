a472d39b4839d3b803f87f1d45b285e513e3aab4
Robin DUBERNARD <robin.dubernard@etu.u-bordeaux.fr>
2022-10-17 22:27:25 +0200

    game_test_rdubernard.c EDIT n°1

--- game_test_rdubernard.c
+++ game_test_rdubernard.c
@@ -3,15 +3,18 @@
 #include<stdbool.h>
 #include<string.h>
 
-void usage(char cmd){
+// USAGE ON ARG N°1
+void usage(char * cmd){
     fprintf(stderr, "Usage %s <testname> [<...>]\n", cmd);
     exit(EXIT_FAILURE);
 }
 
+// TEST_DUMMY
 bool test_dummy(){
     return true;
 }
 
+// MAIN ROUTINE
 int main(int argc, char *argv[]){
     // CMD CONTAIN ONLY EXECUTABLE (0 ARGS)
     if (argc == 1){
@@ -19,25 +22,25 @@ int main(int argc, char *argv[]){
     }
     
     // TEST START
-    fprintf(stderr, "=> Start test : \"%s=\"\n", argv[1]);    
+    fprintf(stderr, "=> Start test : \"%s\"\n", argv[1]);    
     bool ok = false;
 
     // STRING COMPARE ARG N°1
-    if(strcmp("dummy", argv[1])){
+    if(!strcmp("dummy", argv[1])){
         // VALID : "dummy"
-        ok = test_dummy1();
+        ok = test_dummy();
     } else {
         // INVALID : "?"
-        fprintf(stderr, "ERROR : test \"%s\" not found !\n", argv[1]);
+        fprintf(stderr, "=> ERROR : test \"%s\" not found !\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
     // TEST RESULTS
     if (ok){
-        fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
+        fprintf(stderr, "=> Test \"%s\" finished: SUCCESS\n", argv[1]);
         return EXIT_SUCCESS;
     } else {
-        fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
+        fprintf(stderr, "=> Test \"%s\" finished: FAILURE\n", argv[1]);
         return EXIT_SUCCESS;
     }
 }
