6fe81ce147bdb24b6ca9944660b5f02eabd7e12c
Ilisa Bensaad <ilisa.bensaad@etu.u-bordeaux.fr>
2022-10-18 12:21:40 +0200

    Edit des fichiers test ibensaad

--- game_test_ibensaad.c
+++ game_test_ibensaad.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
+
+void usage(char * command){
+    fprintf(stderr,"%s\n",command);
+    exit(EXIT_FAILURE);
+}
+
+bool testDummy(){
+    return true;
+}
+
+int main(int argc, char *argv[]){
+
+    if(argc == 1 || argc > 2){
+        usage(argv[0]);
+    }
+    fprintf(stderr, "->We Start test \"%s\"\n", argv[1]);
+    bool okey = false;
+
+    if(!strcmp(argv[1],"dummy")){
+
+        okey = testDummy();
+
+    }
+    
+    else {
+
+        fprintf(stderr,"-> Error : the name : %s is not found!\n",argv[1]);
+        exit(EXIT_FAILURE);
+
+    }
+
+
+
+    if(okey == true){
+        fprintf(stderr,"Success, test : %s is okey!\n",argv[1]);
+        return EXIT_SUCCESS;
+    }
+
+    if(okey == false){
+        fprintf(stderr,"Fail!, test %s have not succeeded!\n",argv[1]);
+        return EXIT_FAILURE;
+    }
+
+}
