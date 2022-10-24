d6cc4fa30b5da0ed8d58f3739adb37e934ce79d3
Ilisa Bensaad <ilisa.bensaad@etu.u-bordeaux.fr>
2022-10-24 12:38:01 +0200

    update des fichiers game_test_ibensaad implémentation de game_default_solution et CmakeLists link de la librairie et ajouter le test pour default solution (close #20)

--- game_test_ibensaad.c
+++ game_test_ibensaad.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include "game_aux.h"
+#include "game.h"
 
 
 void usage(char * command){
@@ -13,7 +15,17 @@ bool test_dummy(){
     return true;
 }
 
+bool test_default_solution(){
+
+    game g = game_default_solution();
+    //If the party is won, then game_default_solution work and it return true!
+   
+    return game_is_over(g);
+    
+}
+
 int main(int argc, char *argv[]){
+
 
     if(argc == 1 || argc > 2){
         usage(argv[0]);
@@ -21,12 +33,21 @@ int main(int argc, char *argv[]){
     fprintf(stderr, "->We Start test \"%s\"\n", argv[1]);
     bool okey = false;
 
+//Test_dummy:
+
     if(!strcmp(argv[1],"dummy")){
 
         okey = test_dummy();
 
     }
-    
+
+//Test game_default_solution:
+
+    else if(!strcmp(argv[1],"default_solution")){
+        okey = test_default_solution();
+
+    }
+
     else {
 
         fprintf(stderr,"-> Error : the name : %s is not found!\n",argv[1]);
@@ -45,5 +66,14 @@ int main(int argc, char *argv[]){
         fprintf(stderr,"Fail!, test %s have not succeeded!\n",argv[1]);
         return EXIT_FAILURE;
     }
+
+
+    /* game game_default_solution(void);
+       game game_default(void);
+        void game_print(cgame g);
+        void game_restart(game g);
+        void game_play_move(game g, uint i, uint j, square s);
+        bool game_is_over(cgame g);
+    */
 
 }
