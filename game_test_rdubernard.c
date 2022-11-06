9759f030452e5686cd362ed571e5547230ffbff2
Robin Dubernard <robin.dubernard@etu.u-bordeaux.fr>
2022-11-06 18:31:25 +0100

    unit-testing : complete test for test_set_square (dev #27)

--- game_test_rdubernard.c
+++ game_test_rdubernard.c
@@ -110,39 +110,111 @@ bool test_get_square(void){
 }
 
 // ISSUE #6 -> game_set_square()
-bool test_game_set_square(){
-    // GENERATE
+bool test_game_set_square(void){
+    // GENERATE EMPTY GAME
     game g = game_new_empty();
-    ASSERT(g);
     
-    // PUT AND ERASE
-    game_set_square(g, 1, 3, S_ZERO);
-    if(game_get_square(g, 1, 3) != S_ZERO){
+    // FILLING 'b' then 'w' MID
+    for (int x = 0; x < DEFAULT_SIZE; x++){
+        for (int y = 0; y < DEFAULT_SIZE/2; y++){
+            game_set_square(g, x, y, S_ONE);
+        }
+        for (int y = DEFAULT_SIZE/2; y < DEFAULT_SIZE; y++){
+            game_set_square(g, x, y, S_ZERO);
+        }
+    }
+    
+    game_print(g);
+    
+    // VERIF
+    for (int x = 0; x < DEFAULT_SIZE; x++){
+        for (int y = 0; y < DEFAULT_SIZE/2; y++){
+            printf("%d",game_get_square(g, x, y) == S_ONE);
+            if (game_get_square(g, x, y) != S_ONE){
+                game_delete(g);
+                return false;
+            }
+        }
+        for (int y = DEFAULT_SIZE/2; y < DEFAULT_SIZE; y++){
+            printf("%d",game_get_square(g, x, y) == S_ZERO);
+            if (game_get_square(g, x, y) != S_ZERO){
+                game_delete(g);
+                return false;
+            }
+        }
+        printf("\n");
+    }
+    
+    // CLEAR
+    for (int x = 0; x < DEFAULT_SIZE; x++){
+        for (int y = 0; y < DEFAULT_SIZE; y++){
+            game_set_square(g, x, y, S_EMPTY);
+        }
+    }
+
+    game_print(g);
+    
+    // FILLING EACH SQUARE AT RANDOM POS 
+    game_set_square(g, 2, 4, S_IMMUTABLE_ONE);
+    game_set_square(g, 0, 1, S_IMMUTABLE_ZERO);
+    game_set_square(g, 0, 2, S_ONE);
+    game_set_square(g, 0, 3, S_ZERO);
+    game_set_square(g, 0, 4, S_EMPTY);
+    game_set_square(g, 3, 2, S_ONE);
+    game_set_square(g, 5, 1, S_ZERO);
+    game_set_square(g, 3, 4, S_IMMUTABLE_ONE);
+    game_set_square(g, 0, 3, S_EMPTY);
+
+    game_print(g);
+
+    // AND CHECKING IF IT WORKED
+    bool values_matches = 
+        game_get_square(g, 3, 2) == S_ONE &&
+        game_get_square(g, 5, 1) == S_ZERO &&
+        game_get_square(g, 3, 4) == S_IMMUTABLE_ONE &&
+        game_get_next_square(g, 2, 3, RIGHT, 1) == S_IMMUTABLE_ONE &&
+        game_get_square(g, 0, 1) == S_IMMUTABLE_ZERO &&
+        game_get_square(g, 0, 2) == S_ONE &&
+        game_get_square(g, 0, 3) == S_EMPTY &&
+        game_get_square(g, 0, 4) == S_EMPTY;
+
+
+    game_set_square(g, 4, 2, S_ONE);
+    if (game_get_square(g, 4, 2) != S_ONE){
         game_delete(g);
         return false;
     }
 
-    game_set_square(g, 1, 3, S_EMPTY);
-    if(game_get_square(g, 1, 3) != S_EMPTY){
+    game_set_square(g, 4, 2, S_EMPTY);
+    if (game_get_square(g, 4, 2) != S_EMPTY){
         game_delete(g);
         return false;
     }
-    
-    // OTHERS PUT
-    game_set_square(g, 5, 3, S_ONE);
-    game_set_square(g, 2, 5, S_IMMUTABLE_ZERO);
-    game_set_square(g, 4, 1, S_IMMUTABLE_ONE);
-    
-    bool values_matches = 
-        game_get_square(g, 5, 3) == S_ONE &&
-        game_get_square(g, 2, 5) == S_IMMUTABLE_ZERO &&
-        game_get_square(g, 4, 1) == S_IMMUTABLE_ONE;
+
+    game_set_square(g, 1, 5, S_ZERO);
+    if (game_get_square(g, 1, 5) != S_ZERO){
+        game_delete(g);
+        return false;
+    }
+
+    game_set_square(g, 2, 1, S_IMMUTABLE_ONE);
+    if (game_get_square(g, 2, 4) != S_IMMUTABLE_ONE){
+        game_delete(g);
+        return false;
+    }   
+
+    game_set_square(g, 3, 4, S_IMMUTABLE_ZERO);
+    if (game_get_square(g, 3, 4) != S_IMMUTABLE_ZERO){
+        game_delete(g);
+        return false;
+    }
 
     game_delete(g);
+
     if (!values_matches){
         return false;
     }
-  
+
     return true;
 }
 
