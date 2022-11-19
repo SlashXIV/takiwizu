ca40b3cb72e35f4e96d91067e8ad4f0e53c45725
Robin Dubernard <robin.dubernard@etu.u-bordeaux.fr>
2022-11-19 19:40:17 +0100

    implemented v1 of game_new, game_new_empty, game_copy, game_equal, game_delete, game_set_square and game_get_square (dev #28 #29 #30 #31 #32 #34 #35)

--- game.c
+++ game.c
@@ -5,48 +5,105 @@
 #include "game.h"
 #include "game_aux.h"
 
-
 struct game_s{
-
-    square  *ArrayOfSquare;
+    square * ArrayOfSquare;
 
 };
 
 //We have to implement the functions now:
 
-game game_new(square* squares){
+game game_new(square * squares){ // robs
+    if (squares == NULL){
+        fprintf(stderr, "ERROR -> game_new(square * squares) : invalid  squares parameter; pointing on nothing...");
+        exit(EXIT_FAILURE);
+    }
 
-    return NULL;
+    game new_game = malloc(sizeof(game));
+    new_game->ArrayOfSquare = squares;
+    return new_game;
 }
 
-game game_new_empty(void){
+game game_new_empty(void){ // robs
+    uint nb_cases = DEFAULT_SIZE * DEFAULT_SIZE;
+    
+    // automatically generate an empty square array with calloc (0 <=> S_EMPTY)
+    square * squares_empty = calloc(nb_cases, sizeof(square)); 
 
-    return NULL;
+    // using game_new with our empty squares ==> creation of empty game
+    game new_empty_game = game_new(squares_empty);
+
+    return new_empty_game;
 }
 
-game game_copy(cgame g){
+game game_copy(cgame g){ // robs
+    if (g == NULL){
+        fprintf(stderr, "ERROR -> game_copy(cgame g) : invalid g parameter; pointing on nothing...");
+        exit(EXIT_FAILURE);
+    } 
 
-    return NULL;
+    // sending back a new game with the actual state of squares of g
+    return game_new(g->ArrayOfSquare);
 }
 
-bool game_equal(cgame g1, cgame g2){
+bool game_equal(cgame g1, cgame g2){ // robs
+    if (g1 == NULL || g2 == NULL){
+        fprintf(stderr, "ERROR -> game_equal(cgame g1, cgame g2 : one of the two games are pointing on nothing...");
+        exit(EXIT_FAILURE);
+    }
 
+    // traverse and compare every square of both games
+    for (int x = 0; x < DEFAULT_SIZE; x++){
+        for (int y = 0; y < DEFAULT_SIZE; y++){
+            square g1_actual_square = game_get_square(g1, x, y);
+            square g2_actual_square = game_get_square(g2, x, y);
+            if (g1_actual_square != g2_actual_square){
+                // unequal squares on same pos
+                return false;
+            }
+        }
+    }
+    // out of the loop -> identical games 
     return true;
 }
 
-void game_delete(game g){
-
-    //return nothing
+void game_delete(game g){ // robs
+    free(g->ArrayOfSquare);
+    free(g);
 }
 
-void game_set_square(game g, uint i, uint j, square s){
+void game_set_square(game g, uint i, uint j, square s){ // robs
+    if (g == NULL){ 
+        fprintf(stderr, "ERROR on game_set_square(game g, uint i, uint j, square s) : invalid parameters; g pointing on nothing...");
+        exit(EXIT_FAILURE);
+    } else if (i > DEFAULT_SIZE || j > DEFAULT_SIZE) {
+        fprintf(stderr, "ERROR on game_set_square(game g, uint i, uint j, square s) : invalid parameters; i or j aren't under DEFAULT_SIZE (%d) ...", DEFAULT_SIZE);
+        exit(EXIT_FAILURE);
+    } else if (s < S_EMPTY || s > S_IMMUTABLE_ONE) { // s in [0;4]
+        fprintf(stderr, "ERROR on game_set_square(game g, uint i, uint j, square s) : invalid parameters; the square s (%u) isn't a square ...", s);
+        exit(EXIT_FAILURE);
+    }
 
-    //return nothing
+    // CIBLE VERROUILÉE ...
+    uint index_to_set = (i * DEFAULT_SIZE) + j;
+
+    // FIRE (LET THAT F** SQUARE IN) 
+    g->ArrayOfSquare[index_to_set] = s;
 }
 
-square game_get_square(cgame g, uint i, uint j){
+square game_get_square(cgame g, uint i, uint j){ // robs
+    if (g == NULL){ 
+        fprintf(stderr, "ERROR on game_get_square(game g, uint i, uint j : invalid parameters; g pointing on nothing...");
+        exit(EXIT_FAILURE);
+    } else if (i > DEFAULT_SIZE || j > DEFAULT_SIZE) {
+        fprintf(stderr, "ERROR on game_get_square(game g, uint i, uint j, : invalid parameters; i (%u) or j (%u) over DEFAULT_SIZE (%d) ...", i, j, DEFAULT_SIZE);
+        exit(EXIT_FAILURE);
+    }
 
-    return S_EMPTY;
+    // CIBLE VERROUILLÉE ...
+    uint index_to_get = (i * DEFAULT_SIZE) + j;
+
+    // GET THAT SQUARE AND GIVE IT BACK
+    return g->ArrayOfSquare[index_to_get];
 }
 
 int game_get_number(cgame g, uint i, uint j){ //gab
