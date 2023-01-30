6203a7f0fe03a0f7a53247cbb60abfcf7dbfb007
Robin Dubernard <robin.dubernard@etu.u-bordeaux.fr>
2023-01-30 14:52:09 +0100

    game_tools save function v1

--- game_tools.c
+++ game_tools.c
@@ -6,36 +6,24 @@
 #include "game_ext.h"
 #include "game_struct.h"
 
-game game_load(char * filename);
+game game_load(char * filename){
+    return NULL;
+}
 
 void game_save(cgame g, char * filename){
-
     //Creation d'un fichier de sauvegarde
-
     FILE * fgame = fopen(filename, "w");
 
-
-
     //Sauvegarde de la taille du plateau (nb_rows nb_cols wrapping unique\n)
-
-    fprintf(fgame, "%d %d %d %d\n", game_nb_rows(g), game_nb_cols(g), g->wrapping, g->unique);
+    fprintf(fgame, "%d %d %d %d\n", game_nb_rows(g), game_nb_cols(g), game_is_wrapping(g), game_is_unique(g));
 
     //Sauvegarde du plateau de jeu ligne par ligne
-
     for(int i = 0; i < game_nb_rows(g); i++){
-
         for(int j = 0; j < game_nb_cols(g); j++){
-
-            fprintf(fgame, "%d", g->grid[i][j]);
-
+            fprintf(fgame, "%d", game_get_number(g, i, j));
         }
-
         fprintf(fgame, "\n");
-
     }
-
-
-
 }
 
 
