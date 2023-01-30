4acef2e66b6f3008b126c9492c2ff0d5b7b97f1b
Ilisa Bensaad <ilisa.bensaad@etu.u-bordeaux.fr>
2023-01-30 14:44:52 +0100

    correction of some error in implementation tools.c

--- game_tools.c
+++ game_tools.c
@@ -3,6 +3,9 @@
 #include "game_tools.h"
 #include "game.h"
 
+#include "game_ext.h"
+#include "game_struct.h"
+
 game game_load(char * filename);
 
 void game_save(cgame g, char * filename){
@@ -15,15 +18,15 @@ void game_save(cgame g, char * filename){
 
     //Sauvegarde de la taille du plateau (nb_rows nb_cols wrapping unique\n)
 
-    fprintf(fgame, "%d %d %d %d\n", g->nb_rows, g->nb_cols, g->wrapping, g->unique);
+    fprintf(fgame, "%d %d %d %d\n", game_nb_rows(g), game_nb_cols(g), g->wrapping, g->unique);
 
     //Sauvegarde du plateau de jeu ligne par ligne
 
-    for(int i = 0; i < g->nb_rows; i++){
+    for(int i = 0; i < game_nb_rows(g); i++){
 
-        for(int j = 0; j < g->nb_cols; j++){
+        for(int j = 0; j < game_nb_cols(g); j++){
 
-            fprintf(fgame, "%d", g->board[i][j]);
+            fprintf(fgame, "%d", g->grid[i][j]);
 
         }
 
