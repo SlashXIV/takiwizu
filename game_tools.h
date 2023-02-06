3916507130016fc2a93fcf73884ec7c3deee0979
Ilisa Bensaad <ilisa.bensaad@etu.u-bordeaux.fr>
2023-02-06 14:14:34 +0100

    adding game_solve and game_nb_solution in tools.h

--- game_tools.h
+++ game_tools.h
@@ -2,15 +2,16 @@
  * @file game_tools.h
  * @brief Game Tools.
  * @details See @ref index for further details.
- * @copyright University of Bordeaux. All rights reserved, 2022.
+ * @copyright University of Bordeaux. All rights reserved, 2021.
  *
  **/
 
 #ifndef __GAME_TOOLS_H__
 #define __GAME_TOOLS_H__
+#include <stdbool.h>
+#include <stdio.h>
 
 #include "game.h"
-#include "game_struct.h"
 
 /**
  * @name Game Tools
@@ -23,15 +24,32 @@
  * @param filename input file
  * @return the loaded game
  **/
-game game_load(char *filename);
+game game_load(char* filename);
 
 /**
  * @brief Saves a game in a text file.
  * @details See the file format description in @ref index.
  * @param g game to save
- * @param file_path output file
+ * @param filename output file
  **/
-void game_save(cgame g, char *file_path);
+void game_save(cgame g, char* filename);
+
+/**
+ * @brief Computes the solution of a given game
+ * @param g the game to solve
+ * @details The game @p g is updated with the first solution found. If there are
+ * no solution for this game, @p g must be unchanged.
+ * @return true if a solution is found, false otherwise
+ */
+bool game_solve(game g);
+
+/**
+ * @brief Computes the total number of solutions of a given game.
+ * @param g the game
+ * @details The game @p g must be unchanged.
+ * @return the number of solutions
+ */
+uint game_nb_solutions(cgame g);
 
 /**
  * @}
