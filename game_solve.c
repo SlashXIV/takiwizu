6df2ed780df2e568551db0b99995511dc0f5854a
Robin Dubernard <robin.dubernard@etu.u-bordeaux.fr>
2023-02-27 14:45:34 +0100

    game_solve w/ args

---
+++ game_solve.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "game.h"
+#include "annex_funcs.h"
+#include "game_aux.h"
+#include "game_ext.h"
+#include "game_struct.h"
+
+int main(int argc, char *argv[]) {
+  check((argc == 3 || argc == 4), "wrong argument composition");
+
+  char *option = argv[1];
+  char *input = argv[2];
+  if (argc == 4) {
+    bool output_desired = true;
+    char *output = argv[3];
+    FILE * f = fopen(output, "w");
+  }
+
+  game g = game_load(input);
+
+  switch (option) {
+    case "-s":
+      game_solve(g);
+      if (output_desired)
+        game_save(g, output);
+      else
+        game_print(g);
+      break;
+
+    case "-c":
+      int nb_solutions = game_nb_solutions(g);
+      if (output_desired)
+        fprintf(output, "%d", nb_solutions);
+      else
+        printf("%d", nb_solutions);
+      break;
+
+    default:
+      fprintf(stderr, "option saisie invalide %s\n", option);
+      break;
+  }
+
+  return EXIT_SUCCESS;
+}
