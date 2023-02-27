907beea83821f853a5876b428c706b78b8382166
Ilisa Bensaad <ilisa.bensaad@etu.u-bordeaux.fr>
2023-02-27 13:55:41 +0100

    clang

--- game_tools.c
+++ game_tools.c
@@ -21,8 +21,6 @@ square str_to_square[255] = {['e'] = S_EMPTY,
                              ['W'] = S_IMMUTABLE_ZERO,
                              ['B'] = S_IMMUTABLE_ONE};
 
-
-
 game game_load(char* file_path) {
   // Oppening of the saving file:
   FILE* fgame = fopen(file_path, "r");
@@ -68,7 +66,6 @@ void game_save(cgame g, char* filename) {
   fclose(fgame);
 }
 
-
 /*
 void square_fill_black(game g, uint row, uint col) {
   game_set_square(g, row, col, S_ONE);
@@ -105,9 +102,9 @@ bool game_solve(game g) {
             return true;
           }
         }
-        // Backtrack again and return false if the current square cannot be filled with either a white or a black square
-        square_clear(g, row, col);
-        return false;
+        // Backtrack again and return false if the current square cannot be
+filled with either a white or a black square square_clear(g, row, col); return
+false;
       }
     }
   }
@@ -145,17 +142,14 @@ bool game_solve(game ga) {
             return true;
           }
         }
-        // Backtrack again and return false if the current square cannot be filled with either a white or a black square
-        square_clear(g, row, col);
-        return false;
-      } else if (game_get_square(g, row, col) == S_ONE) {
-        black_count_row++;
-      } else if (game_get_square(g, row, col) == S_ZERO) {
-        white_count_row++;
+        // Backtrack again and return false if the current square cannot be
+filled with either a white or a black square square_clear(g, row, col); return
+false; } else if (game_get_square(g, row, col) == S_ONE) { black_count_row++; }
+else if (game_get_square(g, row, col) == S_ZERO) { white_count_row++;
       }
     }
-    if (black_count_row > game_nb_cols(g) / 2 || white_count_row > game_nb_cols(g) / 2) {
-      return false;
+    if (black_count_row > game_nb_cols(g) / 2 || white_count_row >
+game_nb_cols(g) / 2) { return false;
     }
   }
 
@@ -168,13 +162,14 @@ bool game_solve(game ga) {
         white_count_col++;
       }
     }
-    if (black_count_col > game_nb_rows(g) / 2 || white_count_col > game_nb_rows(g) / 2) {
-      return false;
+    if (black_count_col > game_nb_rows(g) / 2 || white_count_col >
+game_nb_rows(g) / 2) { return false;
     }
   }
   printf("Je cherche solution\n");
 
-  //put all in a file with this format : 010101|011010|100101|101001|010101|100110
+  //put all in a file with this format :
+010101|011010|100101|101001|010101|100110
 
   for (row = 0; row < game_nb_rows(g); row++) {
     for (col = 0; col < game_nb_cols(g); col++) {
@@ -185,20 +180,20 @@ bool game_solve(game ga) {
   fclose(fgame);
   game_delete(g);
 
-  
+
   return true;
 }
 
 
 */
 
-void generate_first_solution(int pos, int len, uint *count, bool solved, game solution, game g)
-{
+void generate_first_solution(int pos, int len, uint* count, bool solved,
+                             game solution, game g) {
   // If the game is over, we've found a solution
   if (game_is_over(g)) {
     // Increment the count of solutions found
     (*count)++;
-    
+
     // If this is the first solution we've found and we want to solve the game,
     // copy the current game state into the solution game board
     if (*count == 1 && solved) {
@@ -223,7 +218,8 @@ void generate_first_solution(int pos, int len, uint *count, bool solved, game so
   int j = pos % game_nb_cols(g);
 
   // If the current square is immutable, skip to the next position
-  if (game_get_square(g, i, j) == S_IMMUTABLE_ONE || game_get_square(g, i, j) == S_IMMUTABLE_ZERO) {
+  if (game_get_square(g, i, j) == S_IMMUTABLE_ONE ||
+      game_get_square(g, i, j) == S_IMMUTABLE_ZERO) {
     generate_first_solution(pos + 1, len, count, solved, solution, g);
   } else {
     // Try setting the current square to 1
@@ -243,41 +239,36 @@ void generate_first_solution(int pos, int len, uint *count, bool solved, game so
   }
 }
 
-
-
-bool game_solve(game g){
-
+bool game_solve(game g) {
   check(g != NULL, "game_solve(game g) : g is pointing on nothing");
   FILE* fgame = fopen("solved_game.txt", "w");
   game game_solved = game_copy(g);
   bool solved = true;
-  uint count=0;
-  generate_first_solution(0, game_nb_cols(g) * game_nb_rows(g), &count, solved, game_solved, g);
+  uint count = 0;
+  generate_first_solution(0, game_nb_cols(g) * game_nb_rows(g), &count, solved,
+                          game_solved, g);
 
-  //boucle for pour récupérer les valeurs des cases dans game_solved et les mettre dans un fichier
+  // boucle for pour récupérer les valeurs des cases dans game_solved et les
+  // mettre dans un fichier
 
-  if(count >0){
-
+  if (count > 0) {
     for (int i = 0; i < game_nb_cols(g); i++) {
       for (int j = 0; j < game_nb_rows(g); j++) {
         fprintf(fgame, "%d", game_get_number(game_solved, i, j));
       }
       fprintf(fgame, "|");
-  }
+    }
     fclose(fgame);
     game_delete(game_solved);
     return true;
-  }
-  else{
+  } else {
     fclose(fgame);
     game_delete(game_solved);
     return false;
   }
-
 }
 
-uint generate_all_solutions(int pos, int len, game g)
-{
+uint generate_all_solutions(int pos, int len, game g) {
   // If the game is over, we've found a solution
   if (game_is_over(g)) {
     return 1;
@@ -295,7 +286,8 @@ uint generate_all_solutions(int pos, int len, game g)
   uint count = 0;
 
   // If the current square is immutable, skip to the next position
-  if (game_get_square(g, i, j) == S_IMMUTABLE_ONE || game_get_square(g, i, j) == S_IMMUTABLE_ZERO) {
+  if (game_get_square(g, i, j) == S_IMMUTABLE_ONE ||
+      game_get_square(g, i, j) == S_IMMUTABLE_ZERO) {
     count += generate_all_solutions(pos + 1, len, g);
   } else {
     // Try setting the current square to 1
@@ -317,16 +309,15 @@ uint generate_all_solutions(int pos, int len, game g)
   return count;
 }
 
-
-//FONCTIONNE avec un jeu en 4x4 et default mais à partir de 6x6 (vide) ça charge à l'infini 
+// FONCTIONNE avec un jeu en 4x4 et default mais à partir de 6x6 (vide) ça
+// charge à l'infini
 // a revoir
-uint game_nb_solutions(cgame g)
-{
+uint game_nb_solutions(cgame g) {
   check(g != NULL, "game_nb_solutions(cgame g) : g is pointing on nothing");
-;
+  ;
   game g_copy = game_copy(g);
-  uint count = generate_all_solutions(0, game_nb_cols(g) * game_nb_rows(g), g_copy);
+  uint count =
+      generate_all_solutions(0, game_nb_cols(g) * game_nb_rows(g), g_copy);
   game_delete(g_copy);
   return count;
 }
-
