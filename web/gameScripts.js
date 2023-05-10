b1ab55782111d7ff8a9da3e07ea5435084f5e928
Ilisa Bensaad <ilisa.bensaad@etu.u-bordeaux.fr>
2023-05-10 12:13:32 +0200

    background

--- web/gameScripts.js
+++ web/gameScripts.js
@@ -16,6 +16,7 @@ Module.onRuntimeInitialized = () => {
     }
 
 
+    document.body.style.backgroundImage = "url('background_web_2.jpg')";
 
 
   function processGame(g, canvas, ctx, cell_width, cell_height) {
